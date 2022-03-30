e37a0ee1a30d332f9c539902b57c2d2ab53975a0
mikezalik <mike.zalik@gmail.com>
2022-03-30 14:35:58 -0700

    Adds url for main_app, plants, iabout, base templates, updates model and migration, adds basic styling.

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
+from smtplib import SMTPRecipientsRefused
 from django.db import models
 
 # Create your models here.
+
+class Plant:
+    def __init__(self, name, species, description):
+        self.name: name
+        self.species: species
+        self.description: description
+
+plants = [
+    Plant('Snake Plant', 'Dracaena Trifasciata', 'Slender, vertical, dark green leaves bordered by light green'),
+    Plant('Swiss Cheese Plant', 'Monstera', 'Large leafy greens with holes in them')
+
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import plants as plants
+
+def home(request):
+    return HttpResponse('<h1>Test</h1>')
+
+def about(request):
+    return render(request, 'about.html')
+
+def plants_index(request):
+    return render(request, 'plants/index.html', {
+        'plants': plants,
+    })
 
 # Create your views here.
